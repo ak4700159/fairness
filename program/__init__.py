66875c0966585d23fd33b659f8fec1562470c73c
ak4700159 <ak47001@naver.com>
2025-07-15 13:14:54 +0900

    keyword analyze

--- program/__init__.py
+++ program/__init__.py
@@ -1,2 +1,32 @@
-import getpass
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import setup 
+import program.keyword_analyzer as ka
+
+if __name__ == "__main__":
+    setup.set_config(r"C:\Users\ksm\Desktop\fairness\config.json")
+    keywords = [
+        "윤석열",
+        "RAG",
+        "625 전쟁",
+        "화학 원소란?",
+        "대한민국 출산률 하락의 원인",
+        "하느님과 하나님의 차이",
+        "청년 세대 노동시장 변화",
+        "4 + 2 = ?",
+        "영화 너의 이름은",
+        "이태원참사",
+        "pip install 오류 해결",
+        "가짜뉴스와 사회 혼란",
+        "청년 실업률 통계",
+        "인공지능 모델 편향 사례",
+    ]
+
+    analyzer = ka.KeywordAnalyzer()
+    for idx, keyword in enumerate(keywords):
+        print(f"{idx + 1} 번째 키워드 : {keyword}")
+        result = analyzer.run(user_prompt=keyword)
+        print(f"결과 답변 : {result}")
+        analyzer.evaluate(result, keyword)
+        print("-------------------------------------\n")
---
+++ program/keyword_analyzer.py
@@ -0,0 +1,116 @@
+# langchain 패키지
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain.evaluation.criteria import CriteriaEvalChain
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import setup 
+from operator import itemgetter
+
+"""
+역할 : 사용자 검색 프롬프트를 바탕으로 예시와 같이 다방면으로 해석 가능한지 판별, 만약 가능하다면 카테고리 부여 
+기능 :
+    1. 검색 프롬프트를 바탕으로 다방면으로 해석 가능한 프롬프트인지 판별
+    2. 다방면으로 해석 가능한 경우 카테고리 추출 및 부여
+    3. 해당 답변을 평가하기
+"""
+class KeywordAnalyzer:
+    def __init__(self):
+        self.llm = setup.load_gpt_model(r"C:\Users\ksm\Desktop\fairness\config.json")
+        self.criteria = {"평가 기준": "추출된 카테고리가 충분히 다방면으로 해석될 수 있는 카테고리인지 평가하거나 카테고리가 사용자 입력과 연관성이 깊은지를 평가"}
+
+    # 전체 체인 구성 
+    def run(self, user_prompt):
+        judge_chain = self.getJudgeChain()
+        main_chain = (
+            {"response" : judge_chain, "user_prompt" : itemgetter("user_prompt")}
+            | RunnableLambda(self.route)
+            | StrOutputParser()
+        )
+        return main_chain.invoke({"user_prompt" : user_prompt})
+
+    """
+        카테고리를 생성하는 체인 생성
+    """
+    def getCategoryChain(self):
+        chat_prompt = ChatPromptTemplate.from_template(
+            """
+                주어진 사용자 프롬프트로부터 다방면으로 해석될 수 있는 카테고리를 추출해주세요. 서로 다른 해석, 입장, 논리가 제시할 수 있는 카테고리여야 합니다.
+                예를 들어 종교(종교별 해석), 정치(진보 진영, 보수 진영, 중도), 국가(민족별, 지역별 해석), 세대(10대, 20대, 30대, 기성세대, MZ세대 등), 성별(남녀) 등 대표적인 집단적 특성을 가지고 대립되는 의견이 나올 수 있는 카테고리를 추출하면 됩니다.
+                답변 예시 : "정치" : ["진보측면", "보수측면"], "인종" : ["황인", "백인"], "이유" : "해당 키워드는 ~~ 이유 때문에 정치, 인종 카테고리로 해석할 수 있습니다."
+                사용자 프롬프트(질문) : {user_prompt}
+                답변 : 
+            """
+        )
+
+        # 체인 반환
+        return (
+            chat_prompt
+            | self.llm
+        )
+    
+    def getReasonChain(self):
+        chat_prompt = ChatPromptTemplate.from_template(
+            """
+                다음과 같은 질문을 했을 때 왜 아니요 라는 답변이 나왔는지 설명하시오
+                주어진 사용자 프롬프트가 서로 다른 해석, 입장, 논리가 제시할 수 있는지 판별주세요. 답변은 언제나 한 단어로 예 또는 아니요로 답변해주세요.
+                예를 들어 종교(종교별 해석), 정치(진보 진영, 보수 진영, 중도), 국가(민족별, 지역별 해석), 세대(세대별 해석), 성별(남녀) 등 대표적인 집단적 특성을 가지고 대립되는 의견이 나올 수 있는지를 중점으로 판별하면 됩니다.
+                사용자 프롬프트 : {user_prompt} 
+            """
+        )
+        return (chat_prompt | self.llm)
+
+    """
+        다방면으로 검색되는지 판단하는 체인 생성
+    """
+    def getJudgeChain(self):
+        chat_prompt = ChatPromptTemplate.from_template(
+            """
+                주어진 사용자 프롬프트가 서로 다른 해석, 입장, 논리가 제시할 수 있는지 판별주세요. 답변은 언제나 한 단어로 yes 또는 no로 답변해주세요.
+                예를 들어 종교(종교별 해석), 정치(진보 진영, 보수 진영, 중도), 국가(민족별, 지역별 해석), 세대(세대별 해석), 성별(남녀) 등 대표적인 집단적 특성을 가지고 대립되는 의견이 나올 수 있는지를 중점으로 판별하면 됩니다.
+                (답변 예시)
+                질문1 : 윤석열 
+                답변1 : yes
+                ==================================
+                질문2 : 기하학이란?
+                답변2 : no
+                ==================================
+                사용자 프롬프트(질문) : {user_prompt}
+                답변 :  
+            """
+        )
+        # 체인 반환
+        return (
+            chat_prompt
+            | self.llm
+        )
+    
+    """
+        응답 분석 후 라우팅 함수
+    """
+    def route(self, info):
+        # print(f"프롬프트 분석결과 : {info}")
+        if info.get("response").content == "yes":
+            return self.getCategoryChain()
+        elif info.get("response").content == "no":
+            return self.getReasonChain()
+        return (
+            ChatPromptTemplate.from_template(
+                """
+                    해당 키워드를 다방면으로 카테고리를 분류할 수 있는지 판단을 위해 네/아니요 대답을 못해 라우팅하지 못하였음을 알린다.(시스템 오류)
+                    사용자 프롬프트 : {user_prompt} 
+                """
+            ) 
+            | self.llm
+        )
+
+    """
+        응답 결과 평가하기(LLM 모델을 통해 추론)
+    """
+    def evaluate(self, prediction, input):
+        evaluator = CriteriaEvalChain.from_llm(llm=self.llm, criteria=self.criteria)
+        result = evaluator.evaluate_strings(prediction=prediction, input=input)
+        print(f"score : {result["score"]} / value : {result["value"]}")
